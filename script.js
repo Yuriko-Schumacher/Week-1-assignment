08d67d3ad5ced58ef90c48952b9b54de30916510
Yuriko Anzai Schumacher <yuriko.schumacher@gmail.com>
2021-02-04 13:05:34 -0500

    final commit

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const margin = { t: 50, r: 50, b: 50, l: 50 };
-const size = { w: 800, h: 800 };
-const svg = d3.select("svg");
+const size = { w: 800, h: 550 };
+const svg = d3.select("#map").append("svg");
 
 svg.attr("width", size.w).attr("height", size.h);
-
-// d3.json('data/maps/us-counties.geo.json')
-//     .then(function (mapData) {
-
-// });
 
 Promise.all([
 	d3.json("data/maps/us-counties.geo.json"),
@@ -21,7 +16,7 @@ Promise.all([
 });
 
 function drawMap(mapG, mapData, mortalityData) {
-	let projection = d3.geoMercator().fitSize([size.w, size.h], mapData);
+	let projection = d3.geoAlbersUsa().fitSize([size.w, size.h], mapData);
 	let path = d3.geoPath(projection);
 
 	let pathSel = mapG
@@ -29,13 +24,34 @@ function drawMap(mapG, mapData, mortalityData) {
 		.data(mapData.features)
 		.enter()
 		.append("path")
-		.attr("state", function (d) {
+		.attr("state", (d) => {
 			return d.properties.STATE;
 		})
-		.attr("county", function (d) {
+		.attr("county", (d) => {
 			return d.properties.COUNTY;
 		})
-		.attr("d", function (d) {
+		.attr("d", (d) => {
 			return path(d);
 		});
+
+	let extent = d3.extent(mortalityData, (d) => +d.mortalityRate).reverse();
+	console.log(extent);
+
+	let colorScale = d3
+		.scaleSequential()
+		.domain(extent)
+		.interpolator(d3.interpolateRdYlBu);
+
+	pathSel.style("fill", (d) => {
+		let stateCode = +d.properties.STATE;
+		let countyCode = +d.properties.COUNTY;
+		let county = mortalityData.filter(
+			(el) => +el.state === stateCode && +el.county === countyCode
+		);
+		if (county.length > 0) {
+			county = county[0];
+			return colorScale(+county.mortalityRate);
+		}
+		return "#aaa";
+	});
 }
